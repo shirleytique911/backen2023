a962a8b07e054103edd33c455870e95dcac6f3ab
shirleytique <shirleytique911@gmail.com>
2023-09-09 11:21:16 -0500

    se corrige entrega 2

--- desafio1.js
+++ desafio1.js
@@ -1,85 +1,85 @@
 class ProductManager {
-  constructor() {
-    this.products = [];
-    this.productIdCounter = 1; 
- 
-  }
-
-  addProduct(title, description, price, thumbnail, code, stock) {
+    constructor() {
+      this.products = [];
+      this.productIdCounter = 1; 
    
-    if (!title || !description || !price || !thumbnail || !code || !stock) {
-      console.log("Faltan datos");
-      return;
     }
-
-    // Validacion que el campo "code" no se repita
-    if (this.products.some((product) => product.code === code)) {
-      console.log("Producto con código proporcionado ya existe");
-      return;
+  
+    addProduct(title, description, price, thumbnail, code, stock) {
+     
+      if (!title || !description || !price || !thumbnail || !code || !stock) {
+        console.log("Faltan datos");
+        return;
+      }
+  
+      // Validacion que el campo "code" no se repita
+      if (this.products.some((product) => product.code === code)) {
+        console.log("Producto con código proporcionado ya existe");
+        return;
+      }
+  
+     // ID autoincrementable
+      const newProduct = {
+        id: this.productIdCounter++,
+        title,
+        description,
+        price,
+        thumbnail,
+        code,
+        stock,
+      };
+  
+      // Agregar el producto al arreglo
+      this.products.push(newProduct);
     }
-
-   // ID autoincrementable
-    const newProduct = {
-      id: this.productIdCounter++,
-      title,
-      description,
-      price,
-      thumbnail,
-      code,
-      stock,
-    };
-
-    // Agregar el producto al arreglo
-    this.products.push(newProduct);
+  
+    getProducts() {
+      return this.products;
+    }
   }
-
-  getProducts() {
-    return this.products;
+  
+  // Ejemplo de uso:
+  const productManager = new ProductManager();
+  
+  productManager.addProduct("Manzanas", "Rojas", 2000, "m1", "apple123", 100);
+  productManager.addProduct("Bananos", "Pequeños", 1800, "B2", "banana456", 200);
+  
+  console.log(productManager.getProducts());
+  
+  const productIdToFind = 1; // ID del producto que deseas buscar
+  const foundProduct = productManager.getById(productIdToFind);
+  
+  if (foundProduct) {
+    console.log("Producto encontrado:", foundProduct);
+  } else {
+    console.log("Producto no encontrado.");
   }
-}
-
-// Ejemplo de uso:
-const productManager = new ProductManager();
-
-productManager.addProduct("Manzanas", "Rojas", 2000, "m1", "apple123", 100);
-productManager.addProduct("Bananos", "Pequeños", 1800, "B2", "banana456", 200);
-
-console.log(productManager.getProducts());
-
-const productIdToFind = 1; // ID del producto que deseas buscar
-const foundProduct = productManager.getById(productIdToFind);
-
-if (foundProduct) {
-  console.log("Producto encontrado:", foundProduct);
-} else {
-  console.log("Producto no encontrado.");
-}
-
-
- //? Consulta por # id de producto.
- getById(id) {
-  const product = this.products.find((product) => product.id === id);
-  if (!product) {
-    console.log(`No se encontró un producto con el ID ${id}`);
-    return null; // Puedes devolver null o un mensaje de error según tus necesidades
+  
+  
+   //? Consulta por # id de producto.
+   getById(id) {
+    const product = this.products.find((product) => product.id === id);
+    if (!product) {
+      console.log(`No se encontró un producto con el ID ${id}`);
+      return null; // Puedes devolver null o un mensaje de error según tus necesidades
+    }
+    return product;
   }
-  return product;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//ejercicio shirley tique 1 entrega
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  //ejercicio shirley tique 1 entrega
--- desafio2.js
+++ desafio2.js
@@ -1,50 +1,116 @@
+// {
+//   "type" :"module"
+// }
 
-
-
+const fs = require('fs');
 
 class ProductManager {
-    constructor (){
-        this.path ="./productos.shirley";
+  constructor(filePath) {
+    this.path = filePath;
+    this.products = [];
+    this.productIdCounter = 1;
+  }
+
+  initialize() {
+    try {
+      const data = fs.readFileSync(this.path, 'utf8');
+      this.products = JSON.parse(data);
+      if (Array.isArray(this.products)) {
+        const maxId = Math.max(...this.products.map((product) => product.id), 0);
+        this.productIdCounter = maxId + 1;
+      }
+    } catch (error) {
+      console.error('Error initializing ProductManager:', error.message);
+    }
+  }
+
+  saveToFile() {
+    try {
+      fs.writeFileSync(this.path, JSON.stringify(this.products, null, 2), 'utf8');
+    } catch (error) {
+      console.error('Error saving data to file:', error.message);
+    }
+  }
+
+  addProduct(productData) {
+    const { title, description, price, thumbnail, code, stock } = productData;
+    if (!title || !description || !price || !thumbnail || !code || !stock) {
+      console.log('Faltan datos');
+      return;
     }
 
-
-    static id = 0;
-
-    addProducto =async (title,price,img, code,stock) =>{
-        let ProductNew = {
-            title,
-            price,
-            img,
-            code,
-            stock
-            
-            
-        };
-
-
-        console.log ("Producto nuevo" + ProductNew)
-        console.log (ProductNew)
-
-        await shirley.writeFile(this.path,"Bienvenidos a mi entrega # 2");
+    const newProduct = {
+      id: this.productIdCounter++,
+      title,
+      description,
+      price,
+      thumbnail,
+      code,
+      stock,
     };
+
+    this.products.push(newProduct);
+    this.saveToFile();
+  }
+
+  getProducts() {
+    return this.products;
+  }
+
+  getProductById(id) {
+    const product = this.products.find((product) => product.id === id);
+    return product || null;
+  }
+
+  updateProduct(id, updatedProductData) {
+    const index = this.products.findIndex((product) => product.id === id);
+    if (index !== -1) {
+      // Preserve the original ID
+      updatedProductData.id = id;
+      this.products[index] = updatedProductData;
+      this.saveToFile();
+      return true; // Success
+    }
+    return false; // Product not found
+  }
+
+  deleteProduct(id) {
+    const index = this.products.findIndex((product) => product.id === id);
+    if (index !== -1) {
+      this.products.splice(index, 1);
+      this.saveToFile();
+      return true; // Success
+    }
+    return false; // Product not found
+  }
 }
-const ProductManager =new ProductManager()
 
+const productManager = new ProductManager('products.json');
+productManager.initialize();
 
-ProductManager.addProductos("MANZANAS","2",1000,500,newDate("2023-08-22"))
+// Ejemplo de uso:
+productManager.addProduct({
+  title: 'Manzanas',
+  description: 'Rojas',
+  price: 2000,
+  thumbnail: 'm1',
+  code: 'apple123',
+  stock: 100,
+});
 
-ProductManager.addProductos("BANANOS","3",2000,300,newDate("2023-08-22"))
+const foundProduct = productManager.getProductById(1);
+if (foundProduct) {
+  console.log('Producto encontrado:', foundProduct);
+} else {
+  console.log('Producto no encontrado.');
+}
 
+// Ejemplo de actualización:
+if (productManager.updateProduct(1, { title: 'Manzanas Verdes' })) {
+  console.log('Producto actualizado.');
+}
 
-
-
-
-
-
-
-
-
-
-
-
-// //ejercicio shirly
+// Ejemplo de eliminación:
+if (productManager.deleteProduct(1)) {
+  console.log('Producto eliminado.');
+}
